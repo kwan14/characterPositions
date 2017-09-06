cf8908895cabc29b5499cc9726aa24c0f6cb68d7
kwan14 <mendel.kwan@telus.net>
2017-09-06 19:59:32 +0000

    Modified countLetters to include indices

--- characterPositions.js
+++ characterPositions.js
@@ -1,1 +1,29 @@
-characterPositions.js
+function countLetters(sentence) {
+
+  var letterCount = {};
+
+
+  //var characterArray = sentence.split(" ");
+  //sentence = characterArray.join("");
+
+  for (var i = 0; i < sentence.length; i++) {
+    if (sentence[i] != " ") {
+      var letter = sentence[i];
+      var letterFound = false;
+      for (var key in letterCount) {
+        if (letter == key) {
+          letterCount[key].count += 1;
+          letterCount[key].indices.push(i);
+          letterFound = true;
+        }
+      }
+      if (letterFound == false) {
+        letterCount[letter] = { count : 1, indices : [i] };
+      }
+    }
+  }
+  return letterCount;
+}
+
+
+console.log(countLetters("lighthouse in the house"));
